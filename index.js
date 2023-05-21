e3824e34981a7aee114c8ed732568a6c7e06132b
Zemorath <trentward10@gmail.com>
2023-05-21 17:56:21 -0500

    Sending with Fetch Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    }).then(res => {
+        return res.json()
+    })
+    .then(data => {
+        const body = document.querySelector("body");
+        const h1 = document.createElement("h1");
+        h1.innerHTML = data.id;
+        body.appendChild(h1);
+    })
+    .catch(data => {
+        const body = document.querySelector("body");
+        body.innerHTML = data.message;
+    })
+};
